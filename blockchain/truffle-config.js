db55039be8c4fab2d20c1de6a2bec2564497383f
Vinay Naik <vinay2522@vinaynaik252@gmail.com>
2024-11-04 07:56:00 +0530

    backend is started working

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -5,19 +5,8 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,  // Make sure this is 7545, not 8545
       network_id: "*"
-    },
-    sepolia: {
-      provider: () => new HDWalletProvider(
-        process.env.MNEMONIC,
-        `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-      ),
-      network_id: 11155111,
-      gas: 5500000,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      skipDryRun: true
     }
   },
   compilers: {
